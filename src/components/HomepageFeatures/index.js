006fc6ae33c7877327d6e90476b33c3294b82fae
Sujata Bhandari <sujatabhandari@nchl.local>
2025-03-05 09:47:59 +0545

    initial changes

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -4,7 +4,7 @@ import styles from './styles.module.css';
 
 const FeatureList = [
   {
-    title: 'Easy to Use',
+    title: 'Easy to Us e',
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
